830ba6ecbde85b7a07559486dddc7685fc7651d2
Vitalii Hanych <64318287+VitoMilanese@users.noreply.github.com>
2026-08-28 15:00:35 +0200

    Load shared wager assets on host section

--- src/BadWolfQuiz.Web/TagHelpers/AnonymousSharedWagerAssetsTagHelper.cs
+++ src/BadWolfQuiz.Web/TagHelpers/AnonymousSharedWagerAssetsTagHelper.cs
@@ -3,21 +3,21 @@ using Microsoft.AspNetCore.Razor.TagHelpers;
 namespace BadWolfQuiz.Web.TagHelpers;
 
 [HtmlTargetElement("div", Attributes = "class")]
+[HtmlTargetElement("section", Attributes = "class")]
 public sealed class AnonymousSharedWagerAssetsTagHelper : TagHelper
 {
     public override void Process(TagHelperContext context, TagHelperOutput output)
     {
         var classes = output.Attributes["class"]?.Value?.ToString() ?? string.Empty;
+        var classNames = classes.Split(' ', StringSplitOptions.RemoveEmptyEntries);
 
-        if (classes.Split(' ', StringSplitOptions.RemoveEmptyEntries)
-            .Contains("player-lobby", StringComparer.Ordinal))
+        if (classNames.Contains("player-lobby", StringComparer.Ordinal))
         {
             output.PostContent.AppendHtml(
                 "<script src=\"/js/anonymous-shared-wager-player.js?v=1\"></script>");
         }
 
-        if (classes.Split(' ', StringSplitOptions.RemoveEmptyEntries)
-            .Contains("host-game-board", StringComparer.Ordinal))
+        if (classNames.Contains("host-game-board", StringComparer.Ordinal))
         {
             output.PostContent.AppendHtml(
                 "<script src=\"/js/anonymous-shared-wager-host.js?v=1\"></script>");
