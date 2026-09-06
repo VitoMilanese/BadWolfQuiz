5eea1517d532bd7955adb577fd9950384c19a9ca
Vitalii Hanych <64318287+VitoMilanese@users.noreply.github.com>
2026-09-06 15:21:13 +0200

    dummy

--- src/BadWolfQuiz.Web/TagHelpers/GameplayPolishAssetsTagHelper.cs
+++ src/BadWolfQuiz.Web/TagHelpers/GameplayPolishAssetsTagHelper.cs
@@ -10,7 +10,7 @@ public sealed class GameplayPolishAssetsTagHelper : TagHelper
     public override void Process(TagHelperContext context, TagHelperOutput output)
     {
         output.PreContent.AppendHtml(
-            "<link rel=\"stylesheet\" href=\"/css/gameplay-polish.css?v=1\" />" +
-            "<script src=\"/js/gameplay-polish.js?v=1\"></script>");
+            "<link rel=\"stylesheet\" href=\"/css/gameplay-polish.css?v=2\" />" +
+            "<script src=\"/js/gameplay-polish.js?v=2\"></script>");
     }
 }
