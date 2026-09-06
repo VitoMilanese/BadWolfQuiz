b909f19cda9111d0c5b60f85db337564c0b4a930
Vitalii Hanych <64318287+VitoMilanese@users.noreply.github.com>
2026-09-06 14:58:42 +0200

    Add gameplay polish asset loader

---
+++ src/BadWolfQuiz.Web/TagHelpers/GameplayPolishAssetsTagHelper.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace BadWolfQuiz.Web.TagHelpers;
+
+[HtmlTargetElement("div", Attributes = "data-game-intro-page")]
+[HtmlTargetElement("div", Attributes = "data-game-code,data-player-id,data-final-status")]
+[HtmlTargetElement("div", Attributes = "data-host-gameplay-view")]
+public sealed class GameplayPolishAssetsTagHelper : TagHelper
+{
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        output.PreContent.AppendHtml(
+            "<link rel=\"stylesheet\" href=\"/css/gameplay-polish.css?v=1\" />" +
+            "<script src=\"/js/gameplay-polish.js?v=1\"></script>");
+    }
+}
