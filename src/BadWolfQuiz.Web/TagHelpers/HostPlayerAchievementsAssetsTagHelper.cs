7ecbeecd0d8ab77f678502f9cfda005ebff8f375
Vitalii Hanych <64318287+VitoMilanese@users.noreply.github.com>
2026-09-11 10:28:25 +0200

    Refresh achievement card assets

--- src/BadWolfQuiz.Web/TagHelpers/HostPlayerAchievementsAssetsTagHelper.cs
+++ src/BadWolfQuiz.Web/TagHelpers/HostPlayerAchievementsAssetsTagHelper.cs
@@ -27,7 +27,7 @@ public sealed class HostPlayerAchievementsAssetsTagHelper(
             "data-host-player-achievements-label",
             localizer["Achievements_Title"].Value);
         output.PostContent.AppendHtml(
-            "<link rel=\"stylesheet\" href=\"/css/player-achievements.css?v=6\" />" +
+            "<link rel=\"stylesheet\" href=\"/css/player-achievements.css?v=7\" />" +
             "<script defer src=\"/js/host-player-achievements.js?v=4\"></script>");
     }
 }
