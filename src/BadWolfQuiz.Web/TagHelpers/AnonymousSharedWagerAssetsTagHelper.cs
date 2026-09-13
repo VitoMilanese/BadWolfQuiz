c26709696e808a7c5856a5cddcec0e7a6bf8762d
Vitalii Hanych <64318287+VitoMilanese@users.noreply.github.com>
2026-09-13 19:48:14 +0200

    Bump host wager restyle cache key

--- src/BadWolfQuiz.Web/TagHelpers/AnonymousSharedWagerAssetsTagHelper.cs
+++ src/BadWolfQuiz.Web/TagHelpers/AnonymousSharedWagerAssetsTagHelper.cs
@@ -30,7 +30,7 @@ public sealed class AnonymousSharedWagerAssetsTagHelper : TagHelper
             output.PreElement.AppendHtml(
                 "<link rel=\"stylesheet\" href=\"/css/player-regular-gameplay-host-followup.css?v=6\" />");
             output.PreElement.AppendHtml(
-                "<link rel=\"stylesheet\" href=\"/css/host-normal-wager-restyle.css?v=1\" />");
+                "<link rel=\"stylesheet\" href=\"/css/host-normal-wager-restyle.css?v=2\" />");
             output.PostContent.AppendHtml(
                 "<script src=\"/js/host-gameplay-submit-guard.js?v=4\" data-host-gameplay-submit-guard></script>");
             output.PostContent.AppendHtml(
