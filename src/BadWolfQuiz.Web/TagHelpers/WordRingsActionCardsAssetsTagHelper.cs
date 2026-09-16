c8d0c11a74770d38de119100483c501356f69186
Vitalii Hanych <64318287+VitoMilanese@users.noreply.github.com>
2026-09-16 05:44:04 +0200

    Load Word Rings gameplay follow-up before action patches

--- src/BadWolfQuiz.Web/TagHelpers/WordRingsActionCardsAssetsTagHelper.cs
+++ src/BadWolfQuiz.Web/TagHelpers/WordRingsActionCardsAssetsTagHelper.cs
@@ -16,6 +16,7 @@ public sealed class WordRingsActionCardsAssetsTagHelper : TagHelper
             "<link rel=\"stylesheet\" href=\"/css/word-rings-gameplay-options.css?v=1\" />");
         output.PostElement.AppendHtml(
             "<script src=\"/js/word-rings-gameplay-options.js?v=1\" defer></script>" +
+            "<script src=\"/js/word-rings-gameplay-options-followup.js?v=1\" defer></script>" +
             "<script src=\"/js/word-rings-action-cards-patch.js?v=2\" defer></script>" +
             "<script src=\"/js/word-rings-action-cards-v2.js?v=5\" defer></script>" +
             "<script src=\"/js/word-rings-action-cards-layout.js?v=2\" defer></script>" +
