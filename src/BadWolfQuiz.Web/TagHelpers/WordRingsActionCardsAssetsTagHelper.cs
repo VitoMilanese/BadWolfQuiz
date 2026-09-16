bd2d3fda8df031ff7ace13882d4471ea22aa5b5a
Vitalii Hanych <64318287+VitoMilanese@users.noreply.github.com>
2026-09-16 15:35:32 +0200

    Load Word Rings action achievement hooks

--- src/BadWolfQuiz.Web/TagHelpers/WordRingsActionCardsAssetsTagHelper.cs
+++ src/BadWolfQuiz.Web/TagHelpers/WordRingsActionCardsAssetsTagHelper.cs
@@ -19,6 +19,7 @@ public sealed class WordRingsActionCardsAssetsTagHelper : TagHelper
             "<script src=\"/js/word-rings-gameplay-options-followup.js?v=1\" defer></script>" +
             "<script src=\"/js/word-rings-theme-sync.js?v=1\" defer></script>" +
             "<script src=\"/js/word-rings-action-cards-patch.js?v=2\" defer></script>" +
+            "<script src=\"/js/word-rings-action-achievements.js?v=1\" defer></script>" +
             "<script src=\"/js/word-rings-action-cards-v2.js?v=5\" defer></script>" +
             "<script src=\"/js/word-rings-action-cards-layout.js?v=2\" defer></script>" +
             "<script src=\"/js/word-rings-action-cards-visible-words.js?v=1\" defer></script>");
