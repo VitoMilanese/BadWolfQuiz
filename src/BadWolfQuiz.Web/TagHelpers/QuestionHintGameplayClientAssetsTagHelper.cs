631bcec775006d13d61778d73d5adb03c9b0ec04
Vitalii Hanych <64318287+VitoMilanese@users.noreply.github.com>
2026-09-18 00:45:29 +0200

    Fix hint client asset tag helper imports

--- src/BadWolfQuiz.Web/TagHelpers/QuestionHintGameplayClientAssetsTagHelper.cs
+++ src/BadWolfQuiz.Web/TagHelpers/QuestionHintGameplayClientAssetsTagHelper.cs
@@ -1,4 +1,4 @@
-using BadWolfQuiz.Web.Models;
+using BadWolfQuiz.Web.Pages.Admin.Games;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
